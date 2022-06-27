19c39ebb5cea5ede99fa2e14ed1f9070593e8789
KvShe <zheshe@list.ru>
2022-06-27 15:57:15 +0300

    final homework

---
+++ Exercise_10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+int Remainder(int number)
+{
+    number = number % 10;
+    return(number);
+}
+
+Console.Write("Введите число от 100 до 999: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 100 ^ number > 999)
+{
+    Console.Write("Я же написал - от 100 до 999. Три цифры! Давай заново, я в тебя верю: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+number = number % 100;
+
+Console.WriteLine($"Вторая цифра введённого числа: {number / 10}");
---
+++ Exercise_13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Remainder(int number)
+{
+    number = number % 10;
+    return(number);
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number < 100)
+{
+    Console.Write("Третьей цифры, в заданном числе нет");    
+}
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"Третья цифра в заданном числе: {Remainder(number)}");
+}
